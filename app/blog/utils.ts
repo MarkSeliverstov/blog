0d32520a10c63c34d69c6b8960669e0b1a069903
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-01 00:14:25 +0200

    chore: using gray-matter for parsing mdx

--- app/blog/utils.ts
+++ app/blog/utils.ts
@@ -1,91 +1,70 @@
 import fs from "fs";
+import matter from "gray-matter";
 import path from "path";
 
-type Metadata = {
-  title: string;
-  date: string;
-  summary: string;
-  tags: string;
-  image?: string;
-};
+function getMDXData(dir: string): MdxFile[] {
+  return fs
+    .readdirSync(dir)
+    .filter((file: string) => path.extname(file) === ".mdx")
+    .map((file: string) => {
+      const rawContent: string = fs.readFileSync(path.join(dir, file), "utf-8");
+      const matterResult: matter.GrayMatterFile<string> = matter(rawContent);
 
-function parseFrontmatter(fileContent: string) {
-  let frontmatterRegex = /---\s*([\s\S]*?)\s*---/;
-  let match = frontmatterRegex.exec(fileContent);
-  let frontMatterBlock = match![1];
-  let content = fileContent.replace(frontmatterRegex, "").trim();
-  let frontMatterLines = frontMatterBlock.trim().split("\n");
-  let metadata: Partial<Metadata> = {};
+      const metadata: MdxMetadata = matterResult.data as MdxMetadata;
+      const content: string = matterResult.content;
+      const slug: string = path.basename(file, path.extname(file));
 
-  frontMatterLines.forEach((line) => {
-    let [key, ...valueArr] = line.split(": ");
-    let value = valueArr.join(": ").trim();
-    value = value.replace(/^['"](.*)['"]$/, "$1"); // Remove quotes
-    metadata[key.trim() as keyof Metadata] = value;
-  });
-
-  return { metadata: metadata as Metadata, content };
+      return {
+        metadata,
+        slug,
+        content,
+      };
+    });
 }
 
-function getMDXFiles(dir: string) {
-  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+export function getBlogPosts(): MdxFile[] {
+  const blogPostsPath: string = path.join(
+    process.cwd(),
+    "app",
+    "blog",
+    "posts",
+  );
+  return getMDXData(blogPostsPath);
 }
 
-function readMDXFile(filePath: string) {
-  let rawContent = fs.readFileSync(filePath, "utf-8");
-  return parseFrontmatter(rawContent);
+function getRelativeDate(targetDate: Date): string {
+  const currentDate: Date = new Date();
+  const yearsAgo: number = currentDate.getFullYear() - targetDate.getFullYear();
+  const monthsAgo: number = currentDate.getMonth() - targetDate.getMonth();
+  const daysAgo: number = currentDate.getDate() - targetDate.getDate();
+
+  if (yearsAgo > 0) {
+    return `${yearsAgo}y ago`;
+  }
+  if (monthsAgo > 0) {
+    return `${monthsAgo}mo ago`;
+  }
+  if (daysAgo > 0) {
+    return `${daysAgo}d ago`;
+  }
+  return "Today";
 }
 
-function getMDXData(dir: string) {
-  let mdxFiles = getMDXFiles(dir);
-  return mdxFiles.map((file) => {
-    let { metadata, content } = readMDXFile(path.join(dir, file));
-    let slug = path.basename(file, path.extname(file));
-
-    return {
-      metadata,
-      slug,
-      content,
-    };
-  });
-}
-
-export function getBlogPosts() {
-  return getMDXData(path.join(process.cwd(), "app", "blog", "posts"));
-}
-
-export function formatDate(date: string, includeRelative = false) {
-  let currentDate = new Date();
+export function formatDate(
+  date: string,
+  includeRelative: boolean = true,
+): string {
   if (!date.includes("T")) {
     date = `${date}T00:00:00`;
   }
-  let targetDate = new Date(date);
-
-  let yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
-  let monthsAgo = currentDate.getMonth() - targetDate.getMonth();
-  let daysAgo = currentDate.getDate() - targetDate.getDate();
-
-  let formattedDate = "";
-
-  if (yearsAgo > 0) {
-    formattedDate = `${yearsAgo}y ago`;
-  } else if (monthsAgo > 0) {
-    formattedDate = `${monthsAgo}mo ago`;
-  } else if (daysAgo > 0) {
-    formattedDate = `${daysAgo}d ago`;
-  } else {
-    formattedDate = "Today";
-  }
-
-  let fullDate = targetDate.toLocaleString("en-us", {
+  let targetDate: Date = new Date(date);
+  let fullDate: string = targetDate.toLocaleString("en-us", {
     month: "long",
     day: "numeric",
     year: "numeric",
   });
 
-  if (!includeRelative) {
-    return fullDate;
-  }
-
-  return `${fullDate} (${formattedDate})`;
+  return includeRelative
+    ? `${fullDate} (${getRelativeDate(targetDate)})`
+    : fullDate;
 }
