be07ada9103c9a75bf5fae6da70bb3e8d59b6d96
Mark Seliverstov <smdmrr00@gmail.com>
2024-09-30 22:31:21 +0200

    feat: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Navbar } from "./components/navbar";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -27,11 +28,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+      <body className="text-black bg-white dark:text-white dark:bg-black max-w-xl mx-4 mt-8 lg:mx-auto">
+        <Navbar />
+        <main>{children}</main>
       </body>
     </html>
   );
