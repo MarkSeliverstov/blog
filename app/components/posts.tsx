ea00bbfd1ce898978b3bbc95f829ceb3dfc68324
Mark Seliverstov <smdmrr00@gmail.com>
2024-09-30 23:02:09 +0200

    feat: add posts layout and logic

---
+++ app/components/posts.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import { formatDate, getBlogPosts } from "@/app/blog/utils";
+
+export function BlogPosts() {
+  let allBlogs = getBlogPosts();
+
+  return (
+    <div>
+      {allBlogs
+        .sort( // Sort the posts by date in descending order
+          (a, b) =>
+            new Date(b.metadata.date).getTime() -
+            new Date(a.metadata.date).getTime(),
+        )
+        .map((post) => (
+          <Link
+            key={post.slug}
+            className="flex flex-col space-y-1 mb-4"
+            href={`/blog/${post.slug}`}
+          >
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
+              <p className="text-neutral-600 dark:text-neutral-400 w-[120px] tabular-nums">
+                {formatDate(post.metadata.date, false)}
+              </p>
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+                {post.metadata.title}
+              </p>
+            </div>
+          </Link>
+        ))}
+    </div>
+  );
+}
