e4b9803c23bcf81f0f80799974e1835176eee93b
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-03 00:54:44 +0200

    chore: mdx -> html transformation

---
+++ app/components/code.tsx
@@ -0,0 +1,31 @@
+import "highlight.js/styles/github-dark.css";
+import hljs from "highlight.js";
+import React from "react";
+
+function get_highlighted_code(
+  code: string,
+  className: string | undefined,
+): string {
+  try {
+    if (className !== undefined) {
+      return hljs.highlight(code, {
+        language: className.replace("language-", ""),
+      }).value;
+    }
+    return hljs.highlightAuto(code).value;
+  } catch {
+    console.error(code);
+    return hljs.highlight(code, { language: "plaintext" }).value;
+  }
+}
+
+export default function Code(props: {
+  children: string;
+  className: string;
+}): Readonly<React.ReactNode> {
+  return React.createElement("code", {
+    dangerouslySetInnerHTML: {
+      __html: get_highlighted_code(props.children, props.className),
+    },
+  });
+}
---
+++ app/components/mdx.tsx
@@ -0,0 +1,93 @@
+import { MDXRemote } from "next-mdx-remote/rsc";
+
+import Code from "./code";
+import Link from "next/link";
+import React from "react";
+
+function CustomLink(props: {
+  href: string;
+  children: React.ReactNode;
+}): React.ReactElement {
+  const { href, children, ...rest } = props;
+
+  if (href.startsWith("/")) {
+    return (
+      <Link href={href}>
+        <a {...rest}>{children}</a>
+      </Link>
+    );
+  }
+
+  if (href.startsWith("#")) {
+    return (
+      <a href={href} {...rest}>
+        {children}
+      </a>
+    );
+  }
+
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer" {...rest}>
+      {children}
+    </a>
+  );
+}
+
+function RoundedImage({
+  alt,
+  ...rest
+}: {
+  alt: string;
+  src: string;
+}): React.ReactElement {
+  console.log(rest);
+  return <img alt={alt} {...rest} />;
+}
+
+function slugify(str: string): string {
+  return str
+    .toString()
+    .toLowerCase()
+    .trim() // Remove whitespace from both ends of a string
+    .replace(/\s+/g, "-") // Replace spaces with -
+    .replace(/&/g, "-and-") // Replace & with 'and'
+    .replace(/[^\w\-]+/g, "") // Remove all non-word characters except for -
+    .replace(/\-\-+/g, "-"); // Replace multiple - with single -
+}
+
+function createHeading(level: number) {
+  const Heading = ({ children }: { children: string }): React.ReactElement => {
+    const slug = slugify(children);
+    return React.createElement(
+      `h${level}`,
+      { id: slug },
+      [
+        React.createElement("a", {
+          href: `#${slug}`,
+          key: `link-${slug}`,
+          className: "anchor",
+        }),
+      ],
+      children,
+    );
+  };
+
+  Heading.displayName = `Heading${level}`;
+  return Heading;
+}
+
+const components = {
+  h1: createHeading(1),
+  h2: createHeading(2),
+  h3: createHeading(3),
+  h4: createHeading(4),
+  h5: createHeading(5),
+  h6: createHeading(6),
+  img: RoundedImage,
+  a: CustomLink,
+  code: Code,
+};
+
+export function CustomMDX(props: any): React.ReactElement { // eslint-disable-line
+  return <MDXRemote {...props} components={components} />;
+}
