9ba1693fd23352a31b9de8a97baac21f95b4b402
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-04 01:07:53 +0200

    fix: css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home(): Readonly<React.ReactNode> {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">
+      <h1>
         Welcome to my site
       </h1>
     </section>
