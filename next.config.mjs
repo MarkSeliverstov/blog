8a0b4fcf3e14fd166d1e6e935d9b7ed8ec5e7a56
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-04 14:31:01 +0200

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const nextConfig = {
    * Set base path. This is the slug of your GitHub repository.
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/blog",
+  basePath: "./",
 
   /**
    * Disable server-based image optimization. Next.js does not support
