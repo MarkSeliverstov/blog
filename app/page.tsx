551a23c5f027cc2f5dc4446ec91fe0c9658f28df
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-08 18:58:42 +0200

    fix: urls

--- app/page.tsx
+++ app/page.tsx
@@ -5,14 +5,20 @@ export default function Home(): Readonly<React.ReactNode> {
         <h1 className="md:text-6xl text-4xl">Hello, I&apos;m Mark</h1>
         <p className="md:text-2xl mt-4 text-lg">
           Software Engineer with a passion for back-end development and cloud
-          solutions. If you’re looking to contact me, please see how to get in
-          touch and the ways I’m available to help.
+          solutions. If you’re looking to contact me, you can write me an
+          <a
+            href="mailto:seliverstovmd@gmail.com"
+            className="underline p-1 hover:text-neutral-500"
+          >
+            {"email"}
+          </a>
+          .
         </p>
         <p className="md:text-2xl mt-4 text-lg">
           Also, I have a
           <a
             href="/blog"
-            className="underline p-1 hover:text-neutral-900 dark:hover:text-neutral-100"
+            className="underline p-1 hover:text-neutral-500"
           >
             {"blog"}
           </a>
