7722b03fc5a0e5c46c2af6f030a69501719318f7
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-12 23:37:44 +0200

    chore: add socials

---
+++ app/components/socials.tsx
@@ -0,0 +1,25 @@
+import { SiLinkedin, SiGithub, SiTelegram } from "react-icons/si";
+import { siteMetadata } from "@/app/sitemap";
+
+function createSocialLink(
+  Icon: React.ComponentType,
+  href: string,
+): Readonly<React.ReactNode> {
+  return (
+    <a href={href} target="_blank" rel="noopener noreferrer" className="hover:text-blue-500">
+      <Icon />
+    </a>
+  );
+}
+
+export default function Socials({
+  className,
+}: ClassNameProps): Readonly<React.ReactNode> {
+  return (
+    <div className={"flex flex-row space-x-2 " + className}>
+      {createSocialLink(SiTelegram, siteMetadata.socials.telegram)}
+      {createSocialLink(SiLinkedin, siteMetadata.socials.linkedin)}
+      {createSocialLink(SiGithub, siteMetadata.socials.github)}
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Socials from "@/app/components/socials";
+
 export default function Home(): Readonly<React.ReactNode> {
   return (
     <section className="h-screen">
       <div className="flex justify-center flex-col h-4/6">
-        <h1 className="md:text-6xl text-4xl">Hello, I&apos;m Mark</h1>
-        <p className="md:text-2xl mt-4 text-lg">
+        <h1 className="md:text-6xl text-5xl mb-4">Hello, I&apos;m Mark</h1>
+        <Socials className="text-2xl" />
+        <p className="md:text-2xl mt-8 text-lg">
           Software Engineer with a passion for back-end development and cloud
           solutions. If you’re looking to contact me, you can write me an
           <a
@@ -16,10 +19,7 @@ export default function Home(): Readonly<React.ReactNode> {
         </p>
         <p className="md:text-2xl mt-4 text-lg">
           Also, I have a
-          <a
-            href="/blog"
-            className="underline p-1 hover:text-neutral-500"
-          >
+          <a href="/blog" className="underline p-1 hover:text-neutral-500">
             {"blog"}
           </a>
           where I just started to write my thoughts, tricks and other stuff
