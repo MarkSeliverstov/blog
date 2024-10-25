5dac684d5b6f847fef13fdef389fa2430e3c2b44
Mark Seliverstov <smdmrr00@gmail.com>
2024-10-25 23:53:05 +0200

    feat: prepare project cards

---
+++ app/components/projectCard.tsx
@@ -0,0 +1,44 @@
+import { Tags } from "./tags";
+import Link from "next/link";
+import Image from "next/image";
+
+type props = {
+  project: Project;
+};
+
+export function ProjectCard({ project }: props): Readonly<React.ReactNode> {
+  return (
+    <div>
+      <Link
+        key={project.title}
+        className="flex flex-col space-y-1 mb-2 hover:bg-neutral-200 dark:hover:bg-neutral-800 rounded-lg border border-neutral-200 dark:border-neutral-700"
+        href={project.link}
+      >
+        {project.image && (
+          <Image
+            width={1024}
+            height={1024}
+            src={project.image}
+            className="rounded-t-lg object-cover"
+            alt={project.title}
+          />
+        )}
+        <div className="flex flex-row">
+          <div className="p-4 w-full">
+            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2 md:items-center md:justify-between">
+              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight text-lg">
+                <span>{project.title}</span>
+              </p>
+            </div>
+
+            <p className="text-neutral-600 dark:text-neutral-400 text-sm">
+              {project.description}
+            </p>
+
+            <Tags names={project.tags} />
+          </div>
+        </div>
+      </Link>
+    </div>
+  );
+}
---
+++ app/projects/page.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import { projects } from "@/app/projects/data";
+import { ProjectCard } from "@/app/components/projectCard";
+
+export const metadata: Metadata = { title: "Blog" };
+
+export default function Page(): Readonly<React.ReactNode> {
+  return (
+    <section className="min-h-screen">
+      <p className="text-neutral-900 dark:text-neutral-100 text-lg mb-4">
+        These are some of the projects I've worked on.
+      </p>
+      <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {projects.map((project: Project) => (
+          <ProjectCard key={project.title} project={project} />
+        ))}
+      </div>
+    </section>
+  );
+}
